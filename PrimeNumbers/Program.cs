a3bbf1c4c8db15b33493d67c767f4da4df150ca8
Kurt Weber <Kurt Weber>
2017-03-30 23:16:42 -0400

    first commit

---
+++ PrimeNumbers/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrimeNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // User enters number
+            Console.Write("Please enter a number between 1 and 100: ");
+            String numString = Console.ReadLine();
+            // Convert string to Int
+            int num = Convert.ToInt32(numString);
+
+            // number is frime iff [(n - 1)! + 1]/n is an integer
+            for (int i = 0; i <= 100; i++ )
+            {
+                if 
+            }
+
+
+        }
+    }
+}
