980c4e2a95911af2f0e39c68f741d171cdd7220c
Kurt Weber <Kurt Weber>
2017-04-02 16:53:56 -0400

    Figure out how to remove 1 as prime

--- PrimeNumbers/Program.cs
+++ PrimeNumbers/Program.cs
@@ -10,7 +10,9 @@ namespace PrimeNumbers
     {
         static void Main(string[] args)
         {
+            // Initialize varialbes
             int facNum = 0;
+            int limit = 0;
             // User enters number
             Console.Write("Please enter a number between 1 and 100: ");
             String numString = Console.ReadLine();
@@ -19,22 +21,23 @@ namespace PrimeNumbers
 
             // calculating factorial
             facNum = num - 1;
-            int limit = facNum;
-            Console.WriteLine("num = {0} and facNum = {1}", num, facNum);
-
+            limit = facNum;
             for (int i = 1; i < limit; i++)
             {
-                Console.WriteLine("num = {0} and facNum = {1}", num, facNum);
-                Console.WriteLine("i = {0}",i);
-                facNum = facNum * i;
+              facNum = facNum * i;
             }
 
-            // number is prime iff [(n - 1)! + 1]/n is an integer
+            // Wilson's Theorem: number is prime iff [(n - 1)! + 1]/n is an integer
+            // figure out how to remove 1 as prime
 
-
-
-            Console.WriteLine("num = {0} and facNum = {1}", num, facNum);
-
+            if (((facNum + 1) % num) == 0)
+            {
+                Console.WriteLine("The number {0} is a prime number", num);
+            }
+            else
+            {
+                Console.WriteLine("The number {0} is not a prime number", num);
+            }
         }
     }
 }
