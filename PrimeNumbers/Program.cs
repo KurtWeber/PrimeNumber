51686e5b06a3644fceed2a59f8cfe7b2d40b0447
Kurt Weber <Kurt Weber>
2017-04-09 21:47:59 -0400

    Determining prime numbers by counting modulases

--- PrimeNumbers/Program.cs
+++ PrimeNumbers/Program.cs
@@ -11,32 +11,30 @@ namespace PrimeNumbers
         static void Main(string[] args)
         {
             // Initialize varialbes
-            int facNum = 0;
-            int limit = 0;
+            int num = 0;
+            int divCount = 0;
             // User enters number
             Console.Write("Please enter a number between 1 and 100: ");
             String numString = Console.ReadLine();
             // Convert string to Int
-            int num = Convert.ToInt32(numString);
+            num = Convert.ToInt32(numString);
 
-            // calculating factorial
-            facNum = num - 1;
-            limit = facNum;
-            for (int i = 1; i < limit; i++)
+            // Definition of a prime number - A number that can be divided evenly only by 1, or itself. 
+
+            for (int i = 1; i <= num; i++)
             {
-              facNum = facNum * i;
+                if (num % i == 0)
+                {
+                    divCount = divCount + 1;
+                }
             }
-
-            // Wilson's Theorem: number is prime iff [(n - 1)! + 1]/n is an integer
-            // figure out how to remove 1 as prime
-
-            if (((facNum + 1) % num) == 0 && num != 1)
+            if (divCount <= 2)
             {
-                Console.WriteLine("The number {0} is a prime number", num);
+                Console.WriteLine("{0} is a prime number", num);
             }
-            else
+            else if (divCount > 2)
             {
-                Console.WriteLine("The number {0} is not a prime number", num);
+                Console.WriteLine("{0} is not a prime number", num);
             }
         }
     }
